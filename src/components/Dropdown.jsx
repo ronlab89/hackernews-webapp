d963ee4d8d7bdbaa6044df35a41091ffeeb07ea7
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-05-13 13:52:53 -0400

    Finished dropdown layout

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -7,40 +7,28 @@ import vue from '../assets/images/image-141.png'
 const Dropdown = () => {
   return (
     <section className='drop-section'>
-        {/* <form>
-            <select className='dropdown'>
-                <option className='drop-text' hidden selected>Select your news</option>
-                <option value="angular" className='drop-text'>
-                    <img src={angular} alt="Angular Logo" />
-                    <span>Angular</span>
-                </option>
-                <option value="reactjs" className='drop-text'>
-                    <img src={reactjs} alt="ReactJS Logo" />
-                    <span>Reactjs</span>
-                </option>
-                <option value="vuejs" className='drop-text'>
-                    <img src={vue} alt="VueJS Logo" />
-                    <span>Vuejs</span>
-                </option>
-            </select>
-        </form> */}
-
         <div className="dropdown">
             <span className='drop-text'>Select your news</span>
         </div>
         <input type="checkbox" className='drop-check' id="drop" />
         <ul className='drop-menu'>
             <li className='drop-item'>
-                <img src={angular} alt="Angular Logo" />
-                <span>Angular</span>
+                <div className="drop-framework">
+                    <img src={angular} alt="Angular Logo" />
+                    <span>Angular</span>
+                </div>
             </li>
             <li className='drop-item'>
-                <img src={reactjs} alt="ReactJS Logo" />
-                <span>Reactjs</span>
+                <div className="drop-framework"> 
+                    <img src={reactjs} alt="ReactJS Logo" />
+                    <span>Reactjs</span>
+                </div>
             </li>
             <li className='drop-item'>
-                <img src={vue} alt="VueJS Logo" />
-                <span>Vuejs</span>
+                <div className="drop-framework">
+                    <img src={vue} alt="VueJS Logo" />
+                    <span>Vuejs</span>
+                </div>
             </li>
 
         </ul>
