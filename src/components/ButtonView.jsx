a8537b6a638d30bc2734ce87371cdb911e818a21
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-05-13 10:02:17 -0400

    ButtonView component created and his css file

---
+++ src/components/ButtonView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ButtonView = () => {
+  return (
+    <div>ButtonView</div>
+  )
+}
+
+export default ButtonView
---
+++ src/components/Views.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/css/views.css'
+import ButtonView from './ButtonView'
+
+const Views = () => {
+  return (
+    <section className='views'>
+        <ButtonView />
+    </section>
+  )
+}
+
+export default Views
