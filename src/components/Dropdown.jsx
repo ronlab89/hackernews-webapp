609b369f094734c876e572adf954d91e7312f846
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-05-13 14:08:24 -0400

    dropdown toggle

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../assets/css/dropdown.css'
 import angular from '../assets/images/image-138.png'
 import reactjs from '../assets/images/image-140.png'
 import vue from '../assets/images/image-141.png' 
 
 const Dropdown = () => {
+
+    const [check, setCheck] = useState(false);
+
+    const handleClickDrop = (e) => {
+        e.preventDefault();
+        setCheck(!check);
+    }
+
   return (
     <section className='drop-section'>
-        <div className="dropdown">
+        <div className="dropdown" onClick={handleClickDrop}>
             <span className='drop-text'>Select your news</span>
         </div>
-        <input type="checkbox" className='drop-check' id="drop" />
-        <ul className='drop-menu'>
+        <ul className={`drop-menu ${check ? 'drop-active' : 'drop-disabled'}`}>
             <li className='drop-item'>
                 <div className="drop-framework">
                     <img src={angular} alt="Angular Logo" />
