0b9c43a6fb916db564932b4d61060c3d2019d1d3
Ronald Labrador <ronaldlabrador89@gmail.com>
2022-05-13 09:58:40 -0400

    views component created and his css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+
 import Header from './components/Header'
+import Views from './components/Views'
 
 function App() {
 
   return (
     <main className='main'>
       <Header />
+      <Views />
     </main>
   )
 }
